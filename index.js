ed2ef44dc3ac51e9034202d2a2321a87c8dee8f1
Kelsey Weeks <kelsey@kelseyweeks.com>
2022-06-11 09:11:09 -0500

    commit tree eac4d4cbe2067bf156eedfb6da98fc0b0c77aa08

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+function findMatching (array, name) {
+    let result = array.filter(function(item) {
+        return item.toLowerCase() === name.toLowerCase()
+    })
+    return result
+}
+
+function fuzzyMatch (array, name) {
+    let result = array.filter(function(item) {
+        return item.indexOf(name) === 0
+    })
+    return result
+}
+
+function matchName (array, driver) {
+    let result = array.filter(function(item) {
+        return item.name === driver
+    })
+    return result
+}
